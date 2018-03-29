c759eab2ef785ca80a94415fbf12511ec50277a1
TobWul <tobias.wulvik@gmail.com>
2018-03-29 17:25:59 +0200

    Migration files

--- events/forms.py
+++ events/forms.py
@@ -1,19 +1,9 @@
 from django import forms
 
-from .models import EventOpen, EventRegister
+from .models import Event
 
-class CreateEventOpenForm(forms.ModelForm):
-
-    title = forms.CharField(max_length=150)
+class CreateEventForm(forms.ModelForm):
 
     class Meta:
-        model = EventOpen
-        fields = ['title', 'description', 'event_start_time', 'event_start_date']
-
-class CreateEventRegisterForm(forms.ModelForm):
-
-    title = forms.CharField(max_length=150)
-
-    class Meta:
-        model = EventRegister
-        fields = ['title', 'description', 'event_start_time', 'event_start_date', 'registration_start_date', 'registration_start_time', 'available_spots']
+        model = Event
+        fields = ['title', 'description', 'event_start_time', 'event_start_date', 'registration_start_date', 'registration_start_time','registration_required', 'available_spots']
--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# from .models import Event
+from .models import Event
 
 # Create your views here.
 
