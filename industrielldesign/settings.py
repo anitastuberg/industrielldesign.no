507174ece06eaf1d2f57740d93759aabff5a6102
Tobias <tobias.wulvik@gmail.com>
2018-08-28 19:41:43 +0200

    Added email-support

--- industrielldesign/settings.py
+++ industrielldesign/settings.py
@@ -29,7 +29,13 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'authentication.Profile'
 
-
+# EMAIL
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "webred.leonardo@gmail.com"
+EMAIL_HOST_PASSWORD = "DotGrid99?"
 
 # Application definition
 
