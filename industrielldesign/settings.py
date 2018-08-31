1051583e44224f4bce624336c81a697e8bc0531e
Tobias <tobias.wulvik@gmail.com>
2018-08-31 12:00:22 +0200

    Config tweaking

--- industrielldesign/settings.py
+++ industrielldesign/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from . import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,7 +36,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "webred.leonardo@gmail.com"
-EMAIL_HOST_PASSWORD = "DotGrid99?"
+EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD
+
 
 # Application definition
 
