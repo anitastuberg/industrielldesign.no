356082762f94e31596303627f950fe1c1a347978
Tobias <tobias.wulvik@gmail.com>
2018-09-16 15:23:23 +0200

    Media path fix

--- industrielldesign/settings/base.py
+++ industrielldesign/settings/base.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '..', 'static_in_pro', 'our_static'),
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'static', 'media')
 
 # Django registration redux settings:
