5819eb9e99d630d24302262c420c9369c201097f
Tobias <tobias.wulvik@gmail.com>
2018-10-07 01:44:53 +0200

    Init command MySQL

--- industrielldesign/settings/prod.py
+++ industrielldesign/settings/prod.py
@@ -10,5 +10,8 @@ DATABASES = {
         'USER': 'LeonardoLinjefor',
         'PASSWORD': config.DATABASE_PASSWORD,
         'HOST': 'LeonardoLinjeforening.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'
+        }
     }
 }
