726b91675f886ff784bc274e20b7303e03304d48
Tobias <tobias.wulvik@gmail.com>
2019-02-03 08:55:23 +0100

    Email sender operational

--- authentication/factories.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-import random
-from .models import Profile
-
-fake = Faker('no_NO')
-
-def UserFactory():
-    return Profile.create(email=fake.email(),
-                          first_name=fake.)
