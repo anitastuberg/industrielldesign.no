20016725f969fed28e17f2d43d3611ff86f2ee2a
Olve Steen <olve.steen@gmail.com>
2019-03-12 19:36:53 +0100

    new dynamic-shadow

--- assets/js/dynamicShadow.js
+++ assets/js/dynamicShadow.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
-    let shadowBox = $('.dynamic-shadow');
+    let shadowProject = $('.dynamic-shadow-project');
+    let shadowEvent = $('.dynamic-shadow-event');
 
-    function changeShadow(e) {
+
+    function changeShadow(e, item, sl, spred) {
         let mouseX = e.pageX;
         let mouseY = e.pageY;
-        shadowBox.each(function () {
+        item.each(function () {
             let self = $(this);
             let offset = self.offset();
             let mouse = [mouseX, mouseY];
             let box = [offset.left + self.width() / 2, offset.top + self.height() / 2];
             let l = Math.sqrt(Math.pow((mouse[0] - box[0]), 2) + Math.pow((mouse[1] - box[1]), 2));
             let vf = [-(mouse[0] - box[0]) / l, -(mouse[1] - box[1]) / l];
-            let shadowLength = Math.min(l, 40);
-            let shadow = vf[0] * shadowLength + 'px ' + vf[1] * shadowLength + 'px 70px rgba(0,0,0,0.2)';
+            let shadowLength = Math.min(l, sl);
+            let shadow = vf[0] * shadowLength + 'px ' + vf[1] * shadowLength + 'px ' + spred + 'px rgba(52,79,87,0.3)';
             self.css('box-shadow', shadow);
         })
     }
 
-    document.onmousemove = function (e) {
-        changeShadow(e);
-    };
+    if (window.matchMedia("(max-width: 1000px)")) {
+
+        document.onmousemove = function (e) {
+
+            if (shadowProject.length > 0) {
+                changeShadow(e, shadowProject, 200, 200);
+            }
+            if (shadowProject.length > 0) {
+                changeShadow(e, shadowEvent, 70, 100);
+            }
+        };
+    }
 });
