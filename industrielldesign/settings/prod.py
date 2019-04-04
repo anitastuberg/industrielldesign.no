7745a64e53c90703818443ac1720bc6917adcf07
Tobias <tobias.wulvik@gmail.com>
2019-04-04 14:55:48 +0200

    Changed signature type

--- industrielldesign/settings/prod.py
+++ industrielldesign/settings/prod.py
@@ -41,4 +41,4 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_HOST = 's3.eu-north-1.amazonaws.com'
-AWS_S3_SIGNATURE_VERSION = 'v4'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
