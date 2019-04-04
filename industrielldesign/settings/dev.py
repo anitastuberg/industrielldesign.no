55dea9d05a630c1d3bce38cd53cd9c2de25fef24
Tobias <tobias.wulvik@gmail.com>
2019-04-04 19:33:45 +0200

    Added email password again

--- industrielldesign/settings/dev.py
+++ industrielldesign/settings/dev.py
@@ -1,4 +1,3 @@
-from industrielldesign.settings import config
 from industrielldesign.settings.base import *
 
 # Override base.py settings here
@@ -11,4 +10,4 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), '..', 'media')
-EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD
+EMAIL_HOST_PASSWORD = 'Jwa6dq95xjgvIDsqer'
