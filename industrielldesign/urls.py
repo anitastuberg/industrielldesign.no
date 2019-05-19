e92673589d8c49d6221d1c20106c68982ee9857e
Tobias <tobias.wulvik@gmail.com>
2019-05-19 21:15:23 +0200

    Fixed media url bug

--- industrielldesign/urls.py
+++ industrielldesign/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += staticfiles_urlpatterns()
