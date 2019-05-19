e58f80f19f8d851afa9928f8b53ba8f580d3f3de
Tobias <tobias.wulvik@gmail.com>
2019-05-19 18:04:57 +0200

    Merge error

---
+++ books/views.py
@@ -0,0 +1,45 @@
+from django.http import JsonResponse, HttpResponseForbidden
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from authentication.models import Profile
+from books.models import Book
+from courses.models import Course
+
+
+def books(request):
+    context = {
+        'books': Book.objects.all(),
+    }
+    if request.method == 'GET':
+        return render(request, 'books/books.html', context)
+    else:  # POST
+        # Filter books
+        books = Book.objects.filter(title=request.POST.get("title"))
+        return JsonResponse({
+            'books': books
+        })
+
+
+def create_book(request):
+    context = {}
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'books/create-book.html', context)
+        else:  # POST
+            title = request.POST.get('title')
+            course_code = request.POST.get('course').split(" ")[-1]
+            print(course_code)
+            user = Profile.objects.get(pk=request.user.pk)
+            course = Course.objects.get(course_code=course_code)
+            price = request.POST.get('price')
+            image = request.FILES.get('image')
+            print(image)
+            Book.objects.create(title=title,
+                                seller=user,
+                                course=course,
+                                image=image,
+                                price=price)
+            return redirect('books')
+    else:
+        return HttpResponseForbidden()
