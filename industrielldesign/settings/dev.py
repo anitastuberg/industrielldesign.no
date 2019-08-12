44e46fa1b6d82fded68d3dd843ea713b1acdd9c5
Tobias Wulvik <tobias.wulvik@gmail.com>
2019-08-12 12:01:24 +0200

    fixing error

--- industrielldesign/settings/dev.py
+++ industrielldesign/settings/dev.py
@@ -2,7 +2,7 @@ from industrielldesign.settings.base import *
 
 # Override base.py settings here
 
-DEBUG = True
+DEBUG = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), '..', 'media')
