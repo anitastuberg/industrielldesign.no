898246d546ba6f34da1994c078cce8039a73bbc7
Tobias Wulvik <tobias.wulvik@gmail.com>
2019-08-19 11:54:15 +0200

    fixing event button

--- leonardo/urls.py
+++ leonardo/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('leonardo/komiteer/', views.komiteer, name='komiteer'),
-    path('leonardo/komiteer/<slug:komite_slug>', views.komite_detail, name='komite_detail'),
+    path('leonardo/komiteer/<slug:komite_slug>',
+         views.komite_detail, name='komite_detail'),
     path('leonardo/om-leonardo/', views.about, name='about'),
+    path('leonardo/vedtekter/', views.vedtekter, name='vedtekter'),
     path('leonardo/thesign/', views.thesign, name='thesign'),
 ]
