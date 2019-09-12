0563898ce751bab045c339c9f0bb19b82e91f5e1
Tobias Wulvik <tobias.wulvik@gmail.com>
2019-09-12 10:30:54 +0200

    added folketinget

--- leonardo/urls.py
+++ leonardo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('leonardo/komiteer/', views.komiteer, name='komiteer'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('leonardo/om-leonardo/', views.about, name='about'),
     path('leonardo/vedtekter/', views.vedtekter, name='vedtekter'),
     path('leonardo/thesign/', views.thesign, name='thesign'),
+    path('folketinget', TemplateView.as_view(template_name="leonardo/folketinget.html"), name="folketinget")
 ]
